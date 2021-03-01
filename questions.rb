2a8fb17a1a7062d8d1d78799a29391975556bd9d
vagrant <mveri.bruno@gmail.com>
2021-03-01 23:05:06 +0000

    Correct some type erros

--- questions.rb
+++ questions.rb
@@ -3,11 +3,11 @@ class Questions
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
-    @sum = @sum1 + @sum2
+    @sum = @num1 + @num2
   end
 
   def asking_question(name)
-    puts "#{name}: What does #{num1} plus #{@num2} equal"
+    puts "#{name}: What does #{@num1} plus #{@num2} equal"
   end
 
   def is_correct?(input)
