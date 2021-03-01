9028ab501a0b11092ec893135f3e1fc57e6dba05
vagrant <mveri.bruno@gmail.com>
2021-03-01 22:40:11 +0000

    Added while loop

--- game.rb
+++ game.rb
@@ -8,8 +8,34 @@ class Game
   def initialize
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
+    @current_player = @player1
   end
 
   def play
 
+    while @player1.is_alive? && @player2.is_alive do
+      question = Question.new
+
+      print question.asking_question(@current_player.name)
+      print ">>>"
+
+      user_input = gets.chomp
+
+      if question.is_correct?(user_input.to_i)
+        puts "Yes! You are correct."
+      else
+        puts "Seriously? No!"
+        @current_player.answered_wrong
+      end
+
+      puts "P1: #{@player1.life}/3 vs. P2: #{@player2.life}/3"
+
+      if @current_player == @player1
+        @current_player = @player2
+      else
+        @current_player = @player1
+      end
+        
+      puts "---New Turn---"
     
+    end
