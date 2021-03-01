9cd9274c0d8e366ddfbd6b12489bb43188ec8b5d
vagrant <mveri.bruno@gmail.com>
2021-03-01 22:05:02 +0000

    Added logic to questions

---
+++ questions.rb
@@ -0,0 +1,17 @@
+class Questions
+
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @sum = @sum1 + @sum2
+  end
+
+  def asking(name)
+    puts "#{name}: What does #{num1} plus #{@num2} equal"
+  end
+
+  def is_correct?(input)
+    input == @sum
+  end
+
+end
